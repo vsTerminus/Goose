c5cb936c29ce530236c430f53720394c1aff82a2
vsTerminus <vsTerminus@vsTerminus.ca>
2017-01-05 20:43:35 -0600

    Missed a backslash in the 'set' regex which caused it to match locations not starting with the letter s instead of spaces.

--- Command/Weather.pm
+++ Command/Weather.pm
@@ -295,7 +295,7 @@ sub cmd_weather
             return;
         }
     }
-    elsif ( $args =~ /^set ([^s].*)$/i )
+    elsif ( $args =~ /^set ([^\s].*)$/i )
     {
         my $location = $1;
         $location =~ s/^\<(.*)\>$/$1/; # In case of stupidity, remove < > from the username.
