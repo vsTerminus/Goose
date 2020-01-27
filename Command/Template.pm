5e6806d3795aaf19700d58e3abe299f4cbfcd869
vsTerminus <vsTerminus@vsTerminus.ca>
2020-01-27 11:05:44 -0600

    Rewrote template command with Moo
    The template command now uses Moo, lazy attributes with builders,
    has a simplified pattern with multi-line support, and now demonstrates a
    number of things you might want to do in your command such as:
    
    - Debugging objects
    - Logging
    - Parsing Discord objects and messages
    - Sending a message
    
    This should serve as a much better starting point than the old version.

--- Command/Template.pm
+++ Command/Template.pm
@@ -1,67 +1,53 @@
 package Command::Template;
+use feature 'say';
 
-use v5.10;
-use strict;
-use warnings;
+use Moo;
+use strictures 2;
+
+use namespace::clean;
 
 use Exporter qw(import);
 our @EXPORT_OK = qw(cmd_template);
 
-use Mojo::Discord;
-use Bot::Goose;
+has bot                 => ( is => 'ro' );
+has discord             => ( is => 'lazy', builder => sub { shift->bot->discord } );
+has log                 => ( is => 'lazy', builder => sub { shift->bot->log } );
 
-###########################################################################################
-# Command Info
-my $command = "Template";
-my $access = 0; # Public
-my $description = "This is a template command for building new actual commands";
-my $pattern = '^(template) ?(.*)$';
-my $function = \&cmd_template;
-my $usage = <<EOF;
-Basic usage: !template
-Advanced usage: !template
-Other usage: !template
+has name                => ( is => 'ro', default => 'Template' );
+has access              => ( is => 'ro', default => 0 ); # 0 = Public, 1 = Bot-Owner Only
+has description         => ( is => 'ro', default => 'Template Sample Command' );
+has pattern             => ( is => 'ro', default => '^tem(?:plate)? ?' );
+has function            => ( is => 'ro', default => sub { \&cmd_template } );
+has usage               => ( is => 'ro', default => <<EOF
+This is a template command to demonstrate how to build your own or to use as a starting point.
+
+Basic Usage: !template
+Advanced Usage: !template <your message>
 EOF
-###########################################################################################
-
-sub new
-{
-    my ($class, %params) = @_;
-    my $self = {};
-    bless $self, $class;
-     
-    # Setting up this command module requires the Discord connection 
-    $self->{'bot'} = $params{'bot'};
-    $self->{'discord'} = $self->{'bot'}->discord;
-    $self->{'pattern'} = $pattern;
-
-    # Register our command with the bot
-    $self->{'bot'}->add_command(
-        'command'       => $command,
-        'access'        => $access,
-        'description'   => $description,
-        'usage'         => $usage,
-        'pattern'       => $pattern,
-        'function'      => $function,
-        'object'        => $self,
-    );
-    
-    return $self;
-}
+);
 
 sub cmd_template
 {
     my ($self, $channel, $author, $msg) = @_;
 
+    # "$msg" contains the command and the arguments the user typed.
+    # Most of the time we'll want to strip the command out of $msg and just look at the arguments.
+    # You can use $self->pattern to do this.
     my $args = $msg;
-    my $pattern = $self->{'pattern'};
-    $args =~ s/$pattern/$2/i;
+    my $pattern = $self->pattern;
+    $args =~ s/$pattern//;
+    
+    # Data::Dumper is an easy way to dump any variable, including complex structures, to debug your command. 
+    # You can send its output to the screen or to log files or both.
+    $self->bot->log->debug('[Template.pm] [cmd_template] ' . Data::Dumper->Dump([$msg], ['msg']));
+    say Data::Dumper->Dump([$args], ['args']);
 
-    my $discord = $self->{'discord'};
-    my $replyto = '<@' . $author->{'id'} . '>';
+    # You know the channel the message came from and who sent it.
+    # You can use that information to tailor your reply (eg, mention the user or not, look up other info on them, etc)
+    my $reply = ( length $args ? "Your message was:\n```\n$args\n```" : "Your Discord ID is: " . $author->{'id'} );
 
     # Send a message back to the channel
-    $discord->send_message($channel, "Your message was:\n```$args```");
+    $self->discord->send_message($channel, $reply);
 }
 
 1;
