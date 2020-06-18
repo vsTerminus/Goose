655d80017d9728b816020dff9cbefdbec45f83ab
vsTerminus <vsTerminus@vsTerminus.ca>
2020-06-18 01:43:52 -0500

    Add 1 minute caching to Hoggit component

--- lib/Component/Hoggit.pm
+++ lib/Component/Hoggit.pm
@@ -13,7 +13,7 @@ use namespace::clean;
 use Exporter qw(import);
 our @EXPORT_OK = qw(weather);
 
-has server_urls    => ( is => 'ro', default => sub {
+has server_urls => ( is => 'ro', default => sub {
     {
         'pgaw'  => 'https://pgaw.hoggitworld.com',
         'gaw'   => 'https://dcs.hoggitworld.com',
@@ -25,24 +25,39 @@ has ua          => ( is => 'rw', builder => sub {
         $ua->inactivity_timeout(120);
         return $ua;
     });
+has cache       => ( is => 'rw', default => sub { {} } );
+
 
 # Return the entire JSON message
 sub server_info_p
 {
     my ($self, $server) = @_; # Accepts 'pgaw' or 'gaw', does nothing if undef
 
-    unless (defined $server and (lc $server eq 'pgaw' or lc $server eq 'gaw'))
+    $server = lc $server;
+
+    unless (defined $server and ($server eq 'pgaw' or $server eq 'gaw'))
     {
         carp '$server is undefined. Expected "gaw" or "pgaw".';
         return;
     }
-    my $url = $self->server_urls->{lc $server};
+    my $url = $self->server_urls->{$server};
     my $promise = Mojo::Promise->new;
+
+    if ( exists $self->cache->{$server} and time < $self->cache->{$server}{'expires'} )
+    {
+        $promise->resolve($self->cache->{$server}{'json'});
+        return $promise;
+    }
 
     $promise = $self->ua->get_p($url)->then(sub
         {
             my $tx = shift;
             my $json = $tx->res->json;
+
+            # Cache for 1 minute
+            $self->cache->{$server}{'json'} = $json;
+            $self->cache->{$server}{'expires'} = time+60;
+
             $promise->resolve($json);
         }
     )->catch(sub
