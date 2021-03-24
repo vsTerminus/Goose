a09a2ddbf660a391827eccc4554cacf8430beab4
vsTerminus <vsTerminus@vsTerminus.ca>
2021-03-24 08:17:26 -0500

    Uptime now shows Bot, Session, and Connection uptimes

--- lib/Command/Uptime.pm
+++ lib/Command/Uptime.pm
@@ -3,7 +3,7 @@ use feature 'say';
 
 use Moo;
 use strictures 2;
-
+use Time::Duration;
 use namespace::clean;
 
 use Exporter qw(import);
@@ -25,6 +25,25 @@ Usage: !uptime
 EOF
 );
 
+has 'last_ready'        => ( is => 'rw' );
+has 'last_resumed'      => ( is => 'rw' );
+has 'num_resumed'       => ( is => 'rw', default => 0 );
+has 'num_ready'         => ( is => 'rw', default => 0 );
+has 'created'           => ( is => 'ro', default => time );
+
+has on_ready => ( is => 'ro', default => sub 
+{ 
+    my $self = shift;
+    $self->discord->gw->on('READY' => sub { $self->last_ready(time) });
+});
+
+has on_resumed => ( is => 'ro', default => sub
+{
+    my $self = shift;
+    $self->discord->gw->on('RESUMED' => sub { $self->last_resumed(time) });
+});
+
+
 sub cmd_uptime
 {
     my ($self, $msg) = @_;
@@ -33,8 +52,19 @@ sub cmd_uptime
     my $author = $msg->{'author'};
     my $args = $msg->{'content'};
 
+    my $bot_uptime = duration( time - $self->created );
+    my $session_uptime = duration( time - $self->last_ready );
+    my $connection_uptime = ( $self->last_resumed and $self->last_resumed > $self->last_ready ) ? duration( time - $self->last_resumed ) : duration( time - $self->last_ready );
+
+    # To-Do: Verbose version which also shows READY and RESUMED packet counts, current Sequence Number, and other info?
+
+
     # Send a message back to the channel
-    $self->discord->send_message($channel, ":chart_with_upwards_trend: " . $self->bot->uptime);
+    my $uptime = ":chart_with_upwards_trend: `Bot           $bot_uptime`\n" .
+               ":chart_with_downwards_trend: `Session       $session_uptime`\n" .
+                 ":chart_with_upwards_trend: `Connection    $connection_uptime`\n";
+
+    $self->discord->send_message($channel, $uptime);
 }
 
 1;
